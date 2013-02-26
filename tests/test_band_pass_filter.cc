9aa38bdcd74fb593b8bfddf38d072235206448c1
Joe Turner <joe@oampo.co.uk>
2013-02-26 12:14:08 +0000

    Add test files (just used for making sure everything compiles at the moment)

---
+++ tests/test_band_pass_filter.cc
@@ -0,0 +1,9 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "thelonious/band_pass_filter.h"
+
+int main(int argc, char** argv) {
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ tests/test_unit.cc
@@ -0,0 +1,9 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "thelonious/unit.h"
+
+int main(int argc, char** argv) {
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
