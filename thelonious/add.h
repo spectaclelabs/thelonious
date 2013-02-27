c2e9a54e1079d8c42231fff836898db100a8d2d0
Joe Turner <joe@oampo.co.uk>
2013-02-27 11:47:06 +0000

    Split out operators into seperate files, and add lots of operator overloading...

---
+++ thelonious/add.h
@@ -0,0 +1,122 @@
+#ifndef THELONIOUS_ADD_H
+#define THELONIOUS_ADD_H
+
+#include "types.h"
+#include "unit.h"
+#include "parameter.h"
+
+namespace thelonious {
+
+// Addition (+) operators
+
+// For Channels
+
+template <size_t N>
+Channel<N> operator+(const Channel<N> &a, const Channel<N> &b) {
+    Channel<N> channel;
+    for (int i=0; i<N; i++) {
+        channel[i] = a[i] + b[i];
+    }
+    return channel;
+}
+
+template <size_t N>
+Channel<N> operator+(const Channel<N> &a, Sample b) {
+    Channel<N> channel;
+    for (int i=0; i<N; i++) {
+        channel[i] = a[i] + b;
+    }
+    return channel;
+}
+
+template <size_t N>
+Channel<N> operator+(Sample a, const Channel<N> &b) {
+    return b + a;
+}
+
+// For Buffers
+
+template <size_t M, size_t N>
+Buffer<M, N> operator+(const Buffer<M, N> &a, const Buffer<M, N> &b) {
+    Buffer<M, N> buffer;
+    for (int i=0; i<M; i++) {
+        buffer[i] = a[i] + b[i];
+    }
+    return buffer;
+}
+
+template <size_t M, size_t N>
+Buffer<M, N> operator+(const Buffer<M, N> &a, Sample b) {
+    Buffer<M, N> buffer;
+    for (int i=0; i<M; i++) {
+        buffer[i] = a[i] + b;
+    }
+    return buffer;
+}
+
+template <size_t M, size_t N>
+Buffer<M, N> operator+(Sample a, const Buffer<M, N> &b) {
+    return b + a;
+}
+
+// End addition (+) operators
+
+// Addition assignment (+=) operators
+
+// For Channels
+
+template <size_t N>
+Channel<N> & operator+=(Channel<N> &a, const Channel<N> &b) {
+    for (int i=0; i<N; i++) {
+       a[i] += b[i];
+    }
+    return a;
+}
+
+template <size_t N>
+Channel<N> & operator+=(Channel<N> &a, Sample b) {
+    for (int i=0; i<N; i++) {
+        a[i] += b;
+    }
+    return a;
+}
+
+// For Buffers
+
+template <size_t M, size_t N>
+Buffer<M, N> & operator+=(Buffer<M, N> &a, const Buffer<M, N> &b) {
+    for (int i=0; i<M; i++) {
+        a[i] += b[i];
+    }
+    return a;
+} 
+
+template <size_t M, size_t N>
+Buffer<M, N> & operator+=(Buffer<M, N> &a, Sample b) {
+    for (int i=0; i<M; i++) {
+        a[i] += b;
+    }
+    return a;
+}
+
+// End addition assignment (+=) operators
+
+template <size_t N>
+class Add : public Unit<N> {
+public:
+    Add(Sample value=0.0f): value(value) {}
+
+    void tick(Block<N> &block) {
+        Chock valueChock = value.get();
+
+        for (int i=0; i<N; i++) {
+            block[i] += valueChock;
+        }
+    }
+
+    Parameter value;
+};
+
+}
+
+#endif
---
+++ thelonious/divide.h
@@ -0,0 +1,130 @@
+#ifndef THELONIOUS_DIVIDE_H
+#define THELONIOUS_DIVIDE_H
+
+#include "types.h"
+#include "unit.h"
+#include "parameter.h"
+
+namespace thelonious {
+
+// Division (/) operators
+
+// For Channels
+
+template <size_t N>
+Channel<N> operator/(const Channel<N> &a, const Channel<N> &b) {
+    Channel<N> channel;
+    for (int i=0; i<N; i++) {
+        channel[i] = a[i] / b[i];
+    }
+    return channel;
+}
+
+template <size_t N>
+Channel<N> operator/(const Channel<N> &a, Sample b) {
+    Channel<N> channel;
+    for (int i=0; i<N; i++) {
+        channel[i] = a[i] / b;
+    }
+    return channel;
+}
+
+template <size_t N>
+Channel<N> operator/(Sample a, const Channel<N> &b) {
+    Channel<N> channel;
+    for (int i=0; i<N; i++) {
+        channel[i] = a / b[i];
+    }
+    return channel;
+}
+
+// For Buffers
+
+template <size_t M, size_t N>
+Buffer<M, N> operator/(const Buffer<M, N> &a, const Buffer<M, N> &b) {
+    Buffer<M, N> buffer;
+    for (int i=0; i<M; i++) {
+        buffer[i] = a[i] / b[i];
+    }
+    return buffer;
+}
+
+template <size_t M, size_t N>
+Buffer<M, N> operator/(const Buffer<M, N> &a, Sample b) {
+    Buffer<M, N> buffer;
+    for (int i=0; i<M; i++) {
+        buffer[i] = a[i] / b;
+    }
+    return buffer;
+}
+
+template <size_t M, size_t N>
+Buffer<M, N> operator/(Sample a, const Buffer<M, N> &b) {
+    Buffer<M, N> buffer;
+    for (int i=0; i<M; i++) {
+        buffer[i] = a / b[i];
+    }
+    return buffer;
+}
+
+// End division (/) operators
+
+// Division assignment (/=) operators
+
+// For Channels
+
+template <size_t N>
+Channel<N> & operator/=(Channel<N> &a, const Channel<N> &b) {
+    for (int i=0; i<N; i++) {
+       a[i] /= b[i];
+    }
+    return a;
+}
+
+template <size_t N>
+Channel<N> & operator/=(Channel<N> &a, Sample b) {
+    for (int i=0; i<N; i++) {
+        a[i] /= b;
+    }
+    return a;
+}
+
+// For Buffers
+
+template <size_t M, size_t N>
+Buffer<M, N> & operator/=(Buffer<M, N> &a, const Buffer<M, N> &b) {
+    for (int i=0; i<M; i++) {
+        a[i] /= b[i];
+    }
+    return a;
+} 
+
+template <size_t M, size_t N>
+Buffer<M, N> & operator/=(Buffer<M, N> &a, Sample b) {
+    for (int i=0; i<M; i++) {
+        a[i] /= b;
+    }
+    return a;
+}
+
+// End division assignment (/=) operators
+
+template <size_t N>
+class Divide : public Unit<N> {
+public:
+    Divide(Sample value=0.0f): value(value) {}
+
+    void tick(Block<N> &block) {
+        Chock valueChock = value.get();
+
+        for (int i=0; i<N; i++) {
+            block[i] /= valueChock;
+        }
+    }
+
+    Parameter value;
+};
+
+}
+
+#endif
