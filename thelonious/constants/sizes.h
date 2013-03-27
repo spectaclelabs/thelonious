438a320f12833607989a8ed2a11458c7f09c509a
Joe Turner <joe@oampo.co.uk>
2013-03-27 11:11:58 +0000

    Add inverse block size constant.

--- thelonious/constants/sizes.h
+++ thelonious/constants/sizes.h
@@ -7,6 +7,7 @@ namespace thelonious {
 namespace constants {
 
 const size_t BLOCK_SIZE = 64;
+const float INV_BLOCK_SIZE = 1.0f / BLOCK_SIZE;
 const size_t MAX_CHANNELS = 8;
 
 } // namespace constants
