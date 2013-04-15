29fb047703f0a14fd55e1e4d8dca57a8856ed3e2
Joe Turner <joe@oampo.co.uk>
2013-04-15 10:38:09 +0100

    Remove old tests

--- tests/test_delay.cc
+++
@@ -1,9 +0,0 @@
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
-
-#include "thelonious/dsp/delay.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleMock(&argc, argv);
-    return RUN_ALL_TESTS();
-}
