7e09bc1748f3530a15082a3ea4fd914c262ea4b4
Joe Turner <joe@oampo.co.uk>
2013-08-04 08:14:28 +0100

    Add ID for each unit.

--- thelonious/unit.h
+++ thelonious/unit.h
@@ -5,11 +5,25 @@
 
 namespace thelonious {
 
-template <size_t M, size_t N>
-class Unit {
-public:
-    virtual void tick(Block<M> &inputBlock, Block<N> &outputBlock) = 0;
+// Static members
+template <class Dummy>
+struct UnitStatics {
+    static uint32_t count;
+};
 
+template <class Dummy>
+uint32_t UnitStatics<Dummy>::count = 0;
+
+
+template <size_t M, size_t N>
+class Unit : UnitStatics<void> {
+public:
+    Unit() : id(count) {
+        count++;
+    }
+        
+    virtual void tick(Block<M> &inputBlock, Block<N> &outputBlock) = 0;
+    const uint32_t id;
 };
 
 } // namespace thelonious
